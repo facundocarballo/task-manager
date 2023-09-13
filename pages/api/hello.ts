5d8fc720510a88fae068bd580d72b5bddef96f80
Facundo Carballo <carballofacundo70@gmail.com>
2023-09-13 16:21:51 -0300

    First steps with NextJS API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+import { db } from '@vercel/postgres';
+import { NextApiRequest, NextApiResponse } from 'next';
+ 
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const client = await db.connect();
+ 
+  try {
+    await client.sql`CREATE TABLE IF NOT EXISTS "User" (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );`;
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+ 
+  const users = await client.sql`SELECT * FROM User;`;
+  return response.status(200).json({ users: users.rows });
 }
